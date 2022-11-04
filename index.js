ce8465905fc9a2ec3aa71a5e8375e0706c3ed902
manfredmontero <jmontero0590@icloud.com>
2022-11-03 20:49:03 -0600

    adding second graph

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-import * as UI from './js/ui.js'
-import * as DB from './js/db.js';
-import * as CH from './js/graph.js';
-import * as ST from './js/statistics.js';
+import * as UI from "./js/ui.js";
+import * as DB from "./js/db.js";
+import * as CH from "./js/graph.js";
+import * as ST from "./js/statistics.js";
 
 UI.manageQuestions();
-DB.getSurveys().then(( res )=>{
-    let ages = DB.getSurveyAgeData(res);
-    let groupedAges = ST.groupAgeData( ages );
-    CH.makeAgeChart(groupedAges);
-})
+DB.getSurveys().then((res) => {
+  let ages = DB.getSurveyAgeData(res);
+  let groupedAges = ST.groupAgeData(ages);
+  CH.makeAgeChart(groupedAges);
+  let os = DB.getSurveySOData(res)
+  let osFrecuencies = ST.getFrequencies(os,["Windows", "MacOS", "Linux"])
+  CH.makeOsChart(osFrecuencies)
+});
 
-
-
-document.querySelector('.save').addEventListener('click', () => {
-    DB.postSurvey(UI.buildSurveyObject()).then((survey)=>{
-        
-        
-        console.log(survey);
-    }).catch(err=>console.error(err));
-  });
+document.querySelector(".save").addEventListener("click", () => {
+  DB.postSurvey(UI.buildSurveyObject())
+    .then((survey) => {
+      console.log(survey);
+    })
+    .catch((err) => console.error(err));
+});
