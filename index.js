e4b259c5705e0edb1ad9c8de74b3d89d2fe2f15c
manfredmontero <jmontero0590@icloud.com>
2022-11-03 22:13:13 -0600

    adding fourth  graph

--- index.js
+++ index.js
@@ -11,13 +11,45 @@ DB.getSurveys().then((res) => {
   let os = DB.getSurveySOData(res);
   let osFrecuencies = ST.getFrequencies(os, ["Windows", "MacOS", "Linux"]);
   CH.makeOsChart(osFrecuencies);
-
-  //"HTML","CSS","Javascript","Arrays","DOM","Funciones", "Objetos", "Promesas"
-
   let [siDificulta, noDificulta] = ST.getTopicsFrecuencies(
     DB.getSurveyTopicData(res)
   );
   CH.makeTopicsChart(siDificulta, noDificulta);
+  let countries = DB.getSurveyCountryData(res);
+  console.log(countries);
+  let singleCountries = ST.removeDuplicates(countries);
+  let countryFrecuencies = ST.getFrequencies(countries, singleCountries);
+
+  DB.getCountriesData()
+    .then((res) => {
+      let cordinatesArray = ST.getCoordinatesAndFeq(
+        res,
+        singleCountries,
+        countryFrecuencies
+      );
+      const map = L.map("map").setView([-13.6, -82.5], 3);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+
+      cordinatesArray.forEach((el) => {
+        console.log(el);
+        let circle = L.circle([el[0], el[1]], {
+          color: "red",
+          fillColor: "#f03",
+          fillOpacity: 0.5,
+          stroke: false,
+          radius: 200000 * el[2] * 0.3,
+        }).addTo(map);
+
+        circle.bindTooltip(`${el[2]}`);
+      });
+    })
+
+    .catch((error) => console.log(error));
 });
 
 document.querySelector(".save").addEventListener("click", () => {
