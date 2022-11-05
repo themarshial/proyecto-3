1baaba2917245bb0ea87206c1ef0c3ea661c2e80
Yasser Angel <yasserangelgm@gmail.com>
2022-11-05 07:09:13 -0600

    Delete logic of dashboard construction in index.js

--- js/ui.js
+++ js/ui.js
@@ -1,22 +1,22 @@
 /* export let survey = {}; */
 
-import * as DB from "./db.js";
+import * as DB from './db.js';
 
-let containers = document.querySelectorAll(".contenedor");
+let containers = document.querySelectorAll('.contenedor');
 
 const validateMessages = [
-  "",
-  "Debe escribir un nombre",
-  "Debe escribir su edad",
-  "Debe seleccionar un país",
-  "Seleccione un sistema operativo",
-  "Debe seleccionar al menos un tema",
+  '',
+  'Debe escribir un nombre',
+  'Debe escribir su edad',
+  'Debe seleccionar un país',
+  'Seleccione un sistema operativo',
+  'Debe seleccionar al menos un tema',
 ];
 
 const displayValidateMessage = (message, msgIndex) => {
-  document.querySelectorAll(".validate-message")[msgIndex].innerHTML = message;
+  document.querySelectorAll('.validate-message')[msgIndex].innerHTML = message;
   setTimeout(() => {
-    document.querySelectorAll(".validate-message")[msgIndex].innerHTML = "";
+    document.querySelectorAll('.validate-message')[msgIndex].innerHTML = '';
   }, 1500);
 };
 
@@ -25,16 +25,16 @@ const validateFunctions = [
     return true;
   },
   function () {
-    return document.getElementById("name").value === "" ? false : true;
+    return document.getElementById('name').value === '' ? false : true;
   },
   function () {
-    return document.getElementById("age").value === "" ? false : true;
+    return document.getElementById('age').value === '' ? false : true;
   },
   function () {
-    return document.getElementById("country").value === "" ? false : true;
+    return document.getElementById('country').value === '' ? false : true;
   },
   function () {
-    return Array.from(document.querySelectorAll(".sist_op")).reduce(
+    return Array.from(document.querySelectorAll('.sist_op')).reduce(
       (acc, el) => {
         return acc || el.checked;
       },
@@ -44,7 +44,7 @@ const validateFunctions = [
       : false;
   },
   function () {
-    return Array.from(document.querySelectorAll(".topics")).reduce(
+    return Array.from(document.querySelectorAll('.topics')).reduce(
       (acc, el) => {
         return acc || el.checked;
       },
@@ -58,16 +58,16 @@ const validateFunctions = [
 export const buildSurveyObject = () => {
   let survey = {};
   survey.dificult_topics = [];
-  survey.name = document.getElementById("name").value;
-  survey.age = document.getElementById("age").value;
-  survey.country = document.getElementById("country").value;
-  document.querySelectorAll(".sist_op").forEach((radio) => {
+  survey.name = document.getElementById('name').value;
+  survey.age = document.getElementById('age').value;
+  survey.country = document.getElementById('country').value;
+  document.querySelectorAll('.sist_op').forEach((radio) => {
     if (radio.checked) {
       survey.so = radio.value;
       return;
     }
   });
-  document.querySelectorAll(".topics").forEach((topic) => {
+  document.querySelectorAll('.topics').forEach((topic) => {
     topic.checked
       ? survey.dificult_topics.push(1)
       : survey.dificult_topics.push(0);
@@ -76,13 +76,13 @@ export const buildSurveyObject = () => {
 };
 
 export const manageQuestions = () => {
-  document.querySelectorAll(".next").forEach((boton, index) => {
-    boton.addEventListener("click", () => {
+  document.querySelectorAll('.next').forEach((boton, index) => {
+    boton.addEventListener('click', () => {
       console.log(containers[index]);
       if (validateFunctions[index]()) {
-        containers[index].classList.add("hidden");
+        containers[index].classList.add('hidden');
         if (index + 1 <= containers.length)
-          containers[index + 1].classList.remove("hidden");
+          containers[index + 1].classList.remove('hidden');
       } else {
         displayValidateMessage(validateMessages[index], index);
       }
@@ -91,29 +91,28 @@ export const manageQuestions = () => {
 };
 
 export const showResults = () => {
-  document.querySelectorAll(".contenedor").forEach((e) => {
-    
-    e.classList.add("hidden");
+  document.querySelectorAll('.contenedor').forEach((e) => {
+    e.classList.add('hidden');
   });
-  document.getElementById("graphs-container").classList.remove("hidden");
+  document.getElementById('graphs-container').classList.remove('hidden');
 };
 
 export const showSurvey = () => {
-  document.querySelectorAll(".contenedor").forEach((e) => {
-    e.classList.add("hidden");
+  document.querySelectorAll('.contenedor').forEach((e) => {
+    e.classList.toggle('hidden');
   });
-  document.getElementById("main-container").classList.remove("hidden");
+  /* document.getElementById("main-container").classList.remove("hidden"); */
   clearData();
 };
 
 export const clearData = () => {
-  document.getElementById("name").value = "";
-  document.getElementById("age").value = "";
-  document.getElementById("country").selectedIndex = 0;
-  document.querySelectorAll(".sist_op").forEach((radio) => {
+  document.getElementById('name').value = '';
+  document.getElementById('age').value = '';
+  document.getElementById('country').selectedIndex = 0;
+  document.querySelectorAll('.sist_op').forEach((radio) => {
     radio.checked = false;
   });
-  document.querySelectorAll(".topics").forEach((topic) => {
+  document.querySelectorAll('.topics').forEach((topic) => {
     topic.checked = false;
   });
 };
